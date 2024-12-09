6ee86c2a752246606146db964de45fb1b896ac1b
Ryan Hassan <ryanhassan@Ryans-MacBook-Pro.local>
2024-12-09 11:28:11 +0600

    Fixed Footer

--- src/components/common/footer/footer.jsx
+++ src/components/common/footer/footer.jsx
@@ -6,10 +6,13 @@ const Footer = () => {
   <Fragment>
     <footer className="footer mt-auto xl:ps-[15rem]  font-normal font-inter bg-white text-defaultsize leading-normal text-[0.813] shadow-[0_0_0.4rem_rgba(0,0,0,0.1)] dark:bg-bodybg py-4 text-center">
     <div className="container">
-        <span className="text-gray dark:text-defaulttextcolor/50"> Copyright © <span id="year">2024</span> <a
-                href="#" className="text-defaulttextcolor font-semibold dark:text-defaulttextcolor">Ynex</a>.
-            Designed with <span className="bi bi-heart-fill text-danger"></span> by <a href="#">
-                <span className="font-semibold text-primary underline">Spruko</span>
+        <span className="text-gray dark:text-defaulttextcolor/50"> ©  <span id="year">2024, </span> 
+            Designed by <a             href="https://dma-bd.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="transition-colors hover:text-blue-500 font-bold"
+>
+                <span className="font-semibold text-primary underline">Datasoft Manufacturing & Assembly.</span>
             </a> All
             rights
             reserved
