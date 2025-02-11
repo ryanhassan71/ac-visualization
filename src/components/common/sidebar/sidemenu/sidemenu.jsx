a86c3d051cb17f529fc1b87672ff78f468365ead
unknown <ryanworkdev@gmail.com>
2025-02-11 11:16:52 +0600

    Added DMA office power energy meter

--- src/components/common/sidebar/sidemenu/sidemenu.jsx
+++ src/components/common/sidebar/sidemenu/sidemenu.jsx
@@ -52,9 +52,9 @@ export const MENUITEMS = [
     badgetxt: '',
     class: 'badge !bg-warning/10 !text-warning !py-[0.25rem] !px-[0.45rem] !text-[0.75em] ms-2',
     children: [
-      { path: `${import.meta.env.BASE_URL}4/1/main`, type: 'link', active: false, selected: false, dirchange: false, title: 'Main' },
+      { path: `${import.meta.env.BASE_URL}4/4/main`, type: 'link', active: false, selected: false, dirchange: false, title: 'Main' },
 
-      { path: `${import.meta.env.BASE_URL}4/1/power`, type: 'link', active: false, selected: false, dirchange: false, title: 'Power' },
+      { path: `${import.meta.env.BASE_URL}4/4/power`, type: 'link', active: false, selected: false, dirchange: false, title: 'Power' },
     ]
   },
 
