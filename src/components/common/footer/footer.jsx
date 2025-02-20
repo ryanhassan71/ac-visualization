a4db4df13758f6576635f3f749f55a405b8f26b2
unknown <ryanworkdev@gmail.com>
2025-02-20 15:36:53 +0600

    Made the year current year in footer

--- src/components/common/footer/footer.jsx
+++ src/components/common/footer/footer.jsx
@@ -2,11 +2,12 @@ import {  Fragment } from 'react';
 
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
   return(
   <Fragment>
     <footer className="footer mt-auto xl:ps-[15rem]  font-normal font-inter bg-white text-defaultsize leading-normal text-[0.813] shadow-[0_0_0.4rem_rgba(0,0,0,0.1)] dark:bg-bodybg py-4 text-center">
     <div className="container">
-        <span className="text-gray dark:text-defaulttextcolor/50"> ©  <span id="year">2024, </span> 
+        <span className="text-gray dark:text-defaulttextcolor/50"> ©  <span id="year">{currentYear}, </span> 
             Designed by <a             href="https://dma-bd.com/"
             target="_blank"
             rel="noopener noreferrer"
